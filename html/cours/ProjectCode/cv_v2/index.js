b01ce611ce3976db669df273c8438ea4a49a7360
CatChow0 <thetrueshibadoggo@gmail.com>
2022-10-22 21:40:29 +0200

    にじゅうはち Add screen + start button animated for the showcase and start the pacman game with the shape of pacman.

--- html/cours/ProjectCode/cv_v2/index.js
+++ html/cours/ProjectCode/cv_v2/index.js
@@ -1,4 +1,23 @@
 function show() {
     var element = document.getElementById("showcase");
+    var poutre = document.getElementById("blur");
+    var neonname = document.getElementById("myname");
+    var start = document.getElementById("starting");
+    var pac = document.getElementById("pac");
+
     element.classList.toggle("active");
+    poutre.classList.toggle("active");
+    neonname.classList.toggle("active");
+
+    start.classList.remove("phase2");
+    pac.classList.remove("phase2")
+
+}
+
+function start() {
+    var start = document.getElementById("starting");
+    var pac = document.getElementById("pac")
+
+    start.classList.add("phase2")
+    pac.classList.add("phase2")
 }
---
+++ html/cours/flip-text-animation/README.md
@@ -0,0 +1,4 @@
+# Flip text animation
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/alvarotrigo/pen/bGrXmwM](https://codepen.io/alvarotrigo/pen/bGrXmwM).
+
