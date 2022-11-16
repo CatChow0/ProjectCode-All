838883b3e6fb7e5993697bd2333aadb43f29a679
CatChow0 <thetrueshibadoggo@gmail.com>
2022-11-16 11:49:14 +0100

    py exo Add exo python

---
+++ Python/pytaf/seance3/exo5.py
@@ -0,0 +1,18 @@
+def start():
+    a = str(input('Ecrivez moi: '))
+    a = a[::-1]
+    print(a)
+    a = a[::-1]
+    nb_maj = 0
+    for lettre in a:
+        if lettre.lower() != lettre:
+            nb_maj += 1
+    if nb_maj>0:
+        print("Il y a " + str(nb_maj) + " majuscules.")
+        l = list(a.split(" "))
+        print(l[-1], "est le mot le plus long, et",l[0],"est le mot le plus petit")
+    else:
+        print('Il faut au moins une majuscules')
+        start()
+        
+start()
