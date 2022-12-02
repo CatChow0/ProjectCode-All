61a725c496de81343d42f4322591c3cb9169c16a
CatChow0 <thetrueshibadoggo@gmail.com>
2022-12-02 11:57:01 +0100

    api base

--- NodeJS/todoum/src/App.jsx
+++ NodeJS/todoum/src/App.jsx
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.js';
+import React, {useState} from 'react';
 
 function App() {
+
   return (
     <div className="App">
       
