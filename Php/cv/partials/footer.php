f3da9b1e81db89a12f10183a89afffd91788800a
CatChow0 <thetrueshibadoggo@gmail.com>
2023-03-13 19:33:02 +0100

    n°90 - Clean files/New Online Cv -Delete component "img + desc" , reson : useless. -Renamed "Component" file in "WebComponent" + move out the folder from "Php" -Start the new cv in php

---
+++ Php/cv/partials/footer.php
@@ -0,0 +1,39 @@
+        <footer>
+            <div>
+                <ul class="FootSharedStyle">
+                    <li class="FootTitle">Titre n°1</li>
+                    <li class="FootContent">Text n°1</li>
+                    <li class="FootContent">Text n°2</li>
+                    <li class="FootContent">Text n°3</li>
+                </ul>
+                <ul class="FootSharedStyle">
+                    <li class="FootTitle">Titre n°2</li>
+                    <li class="FootContent">Text n°1</li>
+                    <li class="FootContent">Text n°2</li>
+                    <li class="FootContent">Text n°3</li>
+                </ul>
+                <ul class="FootSharedStyle">
+                    <li class="FootTitle">Contact</li>
+                    <li class="FootContent">
+                        <img src="assets/Instagram.png" alt="insta">
+                        lien insta
+                    </li>
+                    <li class="FootContent">
+                        <img src="assets/Instagram.png" alt="phone">
+                        num tel
+                    </li>
+                    <li class="FootContent">
+                        <img src="assets/Mail.png" alt="mail">
+                        email 
+                    </li>
+                    <li class="FootContent">
+                        <img src="assets/linkedin.png" alt="linkedin">
+                        Nom prenom
+                    </li>
+                </ul>
+            </div>
+            <h5>Copyright here</h5>
+        </footer>
+    <script src="./js/index.js"></script>
+    </body>
+</html>
---
+++ Php/cv/partials/main.php
@@ -0,0 +1,55 @@
+        <head>
+            <link rel="stylesheet" href="./css/style.css">
+        </head>
+        <div class="MainContentArea">
+            <div class="MainContent">
+                <h2 id="WIM">
+                    Qui suis-je?
+                </h2>
+                <div class="SubContentArea">
+
+                </div>
+                <h2 id="Formation">
+                    Formation
+                </h2>
+                <div class="SubContentArea">
+                    d
+                </div>
+                <h2 id="Project">
+                    Projet réalisé
+                </h2>
+                <div class="SubContentArea Project">
+                    <div class="card">
+                        <h3>title</h3>
+                        <img src="./assets/TruenoWp.png" alt="screen">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quis perspiciatis, eaque dolorum qui ratione quasi eum modi illum tempore obcaecati ea consequuntur sapiente cupiditate! Repellendus amet explicabo excepturi maxime.
+                        </p>
+                        <button>
+                            Voir plus
+                        </button>
+                    </div>
+                    <div class="card">
+                        <h3>title</h3>
+                        <img src="./assets/TruenoWp.png" alt="screen">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quis perspiciatis, eaque dolorum qui ratione quasi eum modi illum tempore obcaecati ea consequuntur sapiente cupiditate! Repellendus amet explicabo excepturi maxime.
+                        </p>
+                        <button>
+                            Voir plus
+                        </button>
+                    </div>
+                    <div class="card">
+                        <h3>title</h3>
+                        <img src="./assets/TruenoWp.png" alt="screen">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quis perspiciatis, eaque dolorum qui ratione quasi eum modi illum tempore obcaecati ea consequuntur sapiente cupiditate! Repellendus amet explicabo excepturi maxime.
+                        </p>
+                        <button>
+                            Voir plus
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
